bda82a3db975ac341fa003d0e1459e91e6a9eba5
Kioshi Kiyan Oshiro <kioshi.kiyan@pucp.edu.pe>
2025-06-07 08:45:14 +0900

    update: setting up files for backend

--- backend/main.py
+++ backend/main.py
@@ -142,4 +142,4 @@ async def get_chat_stats():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
